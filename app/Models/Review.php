c5a62b579b15da88fed479d7299772254553985b
hask777 <haskellisp@gmail.com>
2023-08-05 12:14:09 +0300

    scope Model methods

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,11 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
-    
-    protected $fillable = ['review', 'rating'];
-
-    public function book()
-    {
-        return $this->belongsTo(Book::class);
-    }
 }
