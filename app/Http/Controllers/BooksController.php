ed7781c836597fd9ea51c22155fdc140e33c0793
hask777 <haskellisp@gmail.com>
2023-08-08 14:36:01 +0300

    фильтрация

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -49,9 +49,11 @@ class BooksController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return view('books.show', ['book' => $book->load([
+            'reviews' => fn($query) => $query->latest()
+        ])]);
     }
 
     /**
